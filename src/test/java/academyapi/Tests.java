f7c15f03c94e0b8314296e0d48e7dc3a0a312449
Edward Corregidor <edward.corregidor@blackboard.com>
2020-10-13 23:46:38 -0500

    Exercise is done

--- src/test/java/academyapi/Tests.java
+++ src/test/java/academyapi/Tests.java
@@ -21,26 +21,30 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-public class FirstTest {
+public class Tests {
     private Response _response;
     private String _lastId;
-    private static Logger logger = Logger.getLogger(String.valueOf(FirstTest.class));
+    private static Logger logger = Logger.getLogger(String.valueOf(Tests.class));
+    private final String _mockApiUrl = "https://5f8386306b97440016f4e745.mockapi.io/users/";
+    private final String _email = "elcorregidorc@correo.udistrital.edu.co";
+    private final String _ApiJiraToken = "odLsGl7LeBinegJe4dCu21AD";
+    private final String _JiraAPIUrl = "https://edwardcorregidor.atlassian.net/rest/api/2/issue/";
 
     @Test
     public void verifyTheMockApiEndpointIsEmpty() {
-        _response = (Response) RestAssured.get("https://5f8386306b97440016f4e745.mockapi.io/users");
+        _response = (Response) RestAssured.get(_mockApiUrl);
         Assert.assertEquals(_response.getStatusCode(), 200);
         //check if the body is empty
         if (!_response.body().toString().isEmpty()) {
-            ArrayList<String> list = RestAssured.get("https://5f8386306b97440016f4e745.mockapi.io/users").then().extract().path("id");
+            ArrayList<String> list = RestAssured.get(_mockApiUrl).then().extract().path("id");
             int length = list.size();
             //if the body is not empty, all the user will be deleted
             for (int i = 0; i <= length - 1; i++) {
-                _response = RestAssured.delete("https://5f8386306b97440016f4e745.mockapi.io/users/" + list.get(i));
+                _response = RestAssured.delete(_mockApiUrl + list.get(i));
             }
         }
         //checking again the body does not have any user
-        RestAssured.given().when().get("https://5f8386306b97440016f4e745.mockapi.io/users")
+        RestAssured.given().when().get(_mockApiUrl)
                 .then().statusCode(200).body("$", Matchers.hasSize(0));
     }
 
@@ -48,27 +52,27 @@ public class FirstTest {
     public void createAUserWithThePojoMockApiEndpoint() throws Exception {
         PojoMockApi user = new PojoMockApi(2, "Cristiano1", "Ronaldo", 321312, 13123, "retiro",
                 "cristianito@gmail.com", true, "colombia", 31221);
-        _response = RestAssured.given().contentType("application/json").body(user).when().post("https://5f8386306b97440016f4e745.mockapi.io/users/");
+        _response = RestAssured.given().contentType("application/json").body(user).when().post(_mockApiUrl);
         Assert.assertEquals(_response.getStatusCode(), 201);
     }
 
     @Test
     public void negativeTest() {
 
-        _response = (Response) RestAssured.get("https://5f8386306b97440016f4e745.mockapi.io/users");
+        _response = (Response) RestAssured.get(_mockApiUrl);
         String emailReadyToBeSent = "cristianito@gmail.com";
         Assert.assertEquals(_response.getStatusCode(), 200);
         PojoMockApi user = new PojoMockApi(2, "Cristiano2", "Ronaldo2", 3213122, 131232, "retiro2",
                 emailReadyToBeSent, true, "colombia", 312212);
         if (!_response.body().toString().isEmpty()) {
-            ArrayList<String> list = RestAssured.get("https://5f8386306b97440016f4e745.mockapi.io/users").then().extract().path("email");
+            ArrayList<String> list = RestAssured.get(_mockApiUrl).then().extract().path("email");
             int length = list.size();
             //if the body is not empty, all the user will be deleted
             for (int i = 0; i <= length - 1; i++) {
                 try {
                     String emailToCompare = list.get(i);
                     if (!emailReadyToBeSent.equals(emailToCompare)) {
-                        _response = RestAssured.given().contentType("application/json").body(user).when().post("https://5f8386306b97440016f4e745.mockapi.io/users/");
+                        _response = RestAssured.given().contentType("application/json").body(user).when().post(_mockApiUrl);
                     } else {
                         throw new Exception();
                     }
@@ -90,8 +94,8 @@ public class FirstTest {
         String jsonIssue = "{" + fields.toString() + "}";
         JSONParser parser = new JSONParser();
         JSONObject json = (JSONObject) parser.parse(jsonIssue);
-        _response = RestAssured.given().auth().preemptive().basic("elcorregidorc@correo.udistrital.edu.co", "odLsGl7LeBinegJe4dCu21AD")
-                .contentType("application/json").body(json).when().post("https://edwardcorregidor.atlassian.net/rest/api/2/issue/");
+        _response = RestAssured.given().auth().preemptive().basic(_email, _ApiJiraToken)
+                .contentType("application/json").body(json).when().post(_JiraAPIUrl);
         _lastId = _response.then().contentType("application/json").extract().path("id");
         System.out.println(_lastId);
         Assert.assertEquals(_response.getStatusCode(), 201);
@@ -99,7 +103,7 @@ public class FirstTest {
 
     @Test
     public void bugIdIsHigherThanTheLastOne() {
-        _response = RestAssured.given().auth().preemptive().basic("elcorregidorc@correo.udistrital.edu.co", "odLsGl7LeBinegJe4dCu21AD")
+        _response = RestAssured.given().auth().preemptive().basic(_email, _ApiJiraToken)
                 .contentType("application/json").when().get("https://edwardcorregidor.atlassian.net/rest/api/2/search?jql=project=ApiTest");
         Assert.assertEquals(_response.getStatusCode(), 200);
         List<Integer> allIds = _response.jsonPath().getList("issues.id");
@@ -113,7 +117,7 @@ public class FirstTest {
     @Test
     public void creatingAjiraTicketFromMockApi() throws ParseException {
         String idOfUserYouWant = "1";
-        _response = (Response) RestAssured.get("https://5f8386306b97440016f4e745.mockapi.io/users/" + idOfUserYouWant);
+        _response = (Response) RestAssured.get(_mockApiUrl + idOfUserYouWant);
         String descriptionOfIssue = _response.then().extract().path("name", "lastName");
         String idOfUser = _response.then().extract().path("id");
         Fields fields = new Fields(new Project("API"), "Bug title - description",
@@ -121,8 +125,8 @@ public class FirstTest {
         String jsonIssue = "{" + fields.toString() + "}";
         JSONParser parser = new JSONParser();
         JSONObject json = (JSONObject) parser.parse(jsonIssue);
-        _response = RestAssured.given().auth().preemptive().basic("elcorregidorc@correo.udistrital.edu.co", "odLsGl7LeBinegJe4dCu21AD")
-                .contentType("application/json").body(json).when().post("https://edwardcorregidor.atlassian.net/rest/api/2/issue/");
+        _response = RestAssured.given().auth().preemptive().basic(_email, _ApiJiraToken)
+                .contentType("application/json").body(json).when().post(_JiraAPIUrl);
         Assert.assertEquals(_response.getStatusCode(), 201);
     }
 }
